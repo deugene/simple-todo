280ff101ce651d33e91e405d129c9d974bddde92
Ievgen Samoilenko <eug.sml@gmail.com>
2016-12-16 17:31:41 +0200

    interface fixes

--- dist/client/styles.b2328beb0372c051d06d.bundle.js
+++ dist/client/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{384:function(n,o){},687:function(n,o,c){n.exports=c(384)}},[687]);
+webpackJsonp([1,3],{384:function(n,o){},688:function(n,o,c){n.exports=c(384)}},[688]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
   title = 'Another 1500 Todo App';
