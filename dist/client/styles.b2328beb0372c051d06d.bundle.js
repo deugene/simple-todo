4a4de7792992f58492568bf4897e417b4c997778
Ievgen Samoilenko <eug.sml@gmail.com>
2016-12-18 18:02:27 +0200

    toast notifications

---
+++ dist/client/styles.b2328beb0372c051d06d.bundle.js
@@ -0,0 +1,2 @@
+webpackJsonp([1,3],{390:function(n,o){},699:function(n,o,c){n.exports=c(390)}},[699]);
+//# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
---
+++ dist/server/models/todo.js
@@ -0,0 +1,15 @@
+"use strict";
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+;
+const todoSchema = new mongoose.Schema({
+    todo: String,
+    done: Boolean,
+    user_id: String,
+    added: Number,
+    position: Number,
+    reminder: String
+});
+exports.Todo = mongoose.model('Todo', todoSchema);
+;
+//# sourceMappingURL=todo.js.map
